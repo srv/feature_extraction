466cf7db280ef3765602de5108708f6c689cf219
Stephan Wirth <stephan.wirth@uib.es>
2011-09-22 15:44:58 +0200

    added convenience method for key point drawing

--- include/stereo_feature_extraction/drawing.h
+++ include/stereo_feature_extraction/drawing.h
@@ -15,6 +15,8 @@ namespace stereo_feature_extraction
 
     void drawKeyPoint(cv::Mat& image, const KeyPoint& key_point);
 
+    void drawKeyPoints(cv::Mat& image, const std::vector<KeyPoint>& key_points);
+
     void drawStereoFeatures(cv::Mat& canvas, const cv::Mat& left_image,
             const cv::Mat& right_image,
             const std::vector<StereoFeature>& stereo_features);
--- src/stereo_feature_extraction/drawing.cpp
+++ src/stereo_feature_extraction/drawing.cpp
@@ -8,10 +8,18 @@ namespace stereo_feature_extraction
 
     void drawKeyPoint(cv::Mat& image, const KeyPoint& key_point)
     {
-        cv::Point center(cvRound(key_point.pt.x),
+        cv::Point center(cvRound(key_point.pt.x), 
                         cvRound(key_point.pt.y));
         int radius = cvRound(key_point.size);
         cv::circle(image, center, radius, cv::Scalar(0, 255, 0), 2);
+    }
+
+    void drawKeyPoints(cv::Mat& image, const std::vector<KeyPoint>& key_points)
+    {
+        for (size_t i = 0; i < key_points.size(); ++i)
+        {
+            drawKeyPoint(image, key_points[i]);
+        }
     }
 
     void drawStereoFeatures(cv::Mat& canvas, const cv::Mat& left_image,
