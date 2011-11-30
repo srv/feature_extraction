bed8174f01ed20c9fac246b9457bbcc74f4b07ed
Stephan Wirth <stephan.wirth@uib.es>
2011-11-30 15:19:16 +0100

    beautifying

--- include/feature_matching/matching_methods.h
+++ include/feature_matching/matching_methods.h
@@ -5,28 +5,30 @@
 
 namespace feature_matching
 {
-  namespace matching_methods
-  {
-    /**
-    * Matches two sets of descriptors, searching for the 2 nearest neighbors
-    * in descriptors2 for each element in descriptors1. Matches are accepted
-    * if the ratio of the distance from the first match and the distance from
-    * the second match are below the given threshold or if only one neighbor was found.
-    * \param descriptors1 reference descriptors
-    * \param descriptors2 test descriptors
-    * \param matches vector to store matches
-    * \param threshold the matching threshold
-    * \param match_mask the mask to use to allow matches, if empty, all
-    *        descriptors are matched to each other, if not empty, 
-    *        must be of size descriptors1.rows * descriptors2.rows
-    */
-    void thresholdMatching(const cv::Mat& descriptors1, const cv::Mat& descriptors2, 
-                           double threshold, const cv::Mat& match_mask, std::vector<cv::DMatch>& matches);
+namespace matching_methods
+{
+/**
+ * Matches two sets of descriptors, searching for the 2 nearest neighbors
+ * in descriptors2 for each element in descriptors1. Matches are accepted
+ * if the ratio of the distance from the first match and the distance from
+ * the second match are below the given threshold or if only one neighbor 
+ * was found.
+ * \param descriptors1 reference descriptors
+ * \param descriptors2 test descriptors
+ * \param matches vector to store matches
+ * \param threshold the matching threshold
+ * \param match_mask the mask to use to allow matches, if empty, all
+ *        descriptors are matched to each other, if not empty,
+ *        must be of size descriptors1.rows * descriptors2.rows
+ */
+void thresholdMatching(const cv::Mat& descriptors1, const cv::Mat& descriptors2,
+                       double threshold, const cv::Mat& match_mask, 
+                       std::vector<cv::DMatch>& matches);
 
-    void crossCheckFilter(const std::vector<cv::DMatch>& matches1to2, 
-                          const std::vector<cv::DMatch>& matches2to1,
-                          std::vector<cv::DMatch>& checked_matches);
-  }
+void crossCheckFilter(const std::vector<cv::DMatch>& matches1to2,
+                      const std::vector<cv::DMatch>& matches2to1,
+                      std::vector<cv::DMatch>& checked_matches);
+}
 }
 
 #endif
