52cf2ffafaed2f33f0dc714f835d11921599a1f5
Stephan Wirth <stephan.wirth@uib.es>
2012-09-04 09:51:35 +0200

    OpenCV 2.4 support, added nonfree includes

--- src/feature_extraction/cv_descriptor_extractor.cpp
+++ src/feature_extraction/cv_descriptor_extractor.cpp
@@ -2,11 +2,24 @@
 
 #include <opencv2/features2d/features2d.hpp>
 
+#if (CV_MAJOR_VERSION > 2 ) || ( CV_MAJOR_VERSION == 2 && CV_MINOR_VERSION >= 4)
+#include <opencv2/nonfree/nonfree.hpp>
+#endif
+
 #include "feature_extraction/cv_descriptor_extractor.h"
+
+bool feature_extraction::CvDescriptorExtractor::nonfree_module_initialized_ = false;
 
 feature_extraction::CvDescriptorExtractor::CvDescriptorExtractor(const std::string& name) :
   DescriptorExtractor()
 {
+#if (CV_MAJOR_VERSION > 2 ) || ( CV_MAJOR_VERSION == 2 && CV_MINOR_VERSION >= 4)
+  if (!nonfree_module_initialized_)
+  {
+    cv::initModule_nonfree();
+    nonfree_module_initialized_ = true;
+  }
+#endif
   cv_extractor_ = cv::DescriptorExtractor::create(name);
   if (!cv_extractor_)
   {
